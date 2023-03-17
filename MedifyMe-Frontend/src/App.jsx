d447d61723d3f6feeabb72075032f2b4975b0a77
SaiRev <saiyamjain1234567@gmail.com>
2023-03-17 20:45:19 +0530

    Fixed

--- MedifyMe-Frontend/src/App.jsx
+++ MedifyMe-Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import Home from "./pages/Home";
+import "./App.css";
 
 import {
   createBrowserRouter,
@@ -10,14 +11,20 @@ import {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route index element={<><Home /></>} />
+      <Route
+        index
+        element={
+          <>
+            <Home />
+          </>
+        }
+      />
       {/* <Route path="*" element={<Error404 />} /> */}
     </Route>
   )
 );
 
 function App() {
-
   return <RouterProvider router={router} />;
 }
 
