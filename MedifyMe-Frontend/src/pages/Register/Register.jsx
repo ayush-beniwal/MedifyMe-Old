5094fc2cd6d5cc6e3b27a4700a3932b6d2321af8
Amit Sharma <amit.sharma.mec21@itbhu.ac.in>
2023-03-19 00:15:16 +0530

    implemented the chatbot

--- MedifyMe-Frontend/src/pages/Register/Register.jsx
+++ MedifyMe-Frontend/src/pages/Register/Register.jsx
@@ -1,17 +1,34 @@
 import styles from "./Register.module.css";
 import useChatGPT from "../../hooks/useChatGPT";
 import Navbar from "../../components/Navbar/Navbar";
+import { useEffect, useRef } from "react";
 
 function Register() {
   const { messages, handleSend } = useChatGPT();
+
+  const messageListRef = useRef(null);
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    messageListRef.current.lastChild.scrollIntoView();
+    inputRef.current.focus();
+  }, [messages]);
+
+  const handleButtonClick = () => {
+    const inputValue = inputRef.current.value;
+    handleSend(inputValue);
+    inputRef.current.value = "";
+  };
+
   return (
     <div className={styles.chatContainer}>
       <Navbar />
-      <div className={styles.messageList}>
+      <div className={styles.title}>Tell us about yourself</div>
+      <div className={styles.messageList} ref={messageListRef}>
         {messages.map((message, i) => (
           <div
             key={i}
-            className={`${styles.message} ${
+            className={`${
               message.sender === "ChatGPT" ? styles.incoming : styles.outgoing
             }`}
           >
@@ -22,18 +39,17 @@ function Register() {
       <div className={styles.messageInput}>
         <input
           type="text"
-          placeholder="Type your message here"
+          placeholder="Enter your message here"
           onKeyPress={(e) => {
             if (e.key === "Enter") {
               handleSend(e.target.value);
               e.target.value = "";
             }
           }}
+          ref={inputRef}
         />
-        <button
-          onClick={() => handleSend(document.querySelector("input").value)}
-        >
-          Send
+        <button onClick={handleButtonClick} className={styles.button}>
+          <img src="image8.svg" />
         </button>
       </div>
     </div>
