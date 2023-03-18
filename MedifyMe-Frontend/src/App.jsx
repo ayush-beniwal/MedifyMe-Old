47dbe3bbd8cc49354cd0e657f8b498792791d5b9
Amit Sharma <amit.sharma.mec21@itbhu.ac.in>
2023-03-18 19:04:51 +0530

    updated App.jsx

--- MedifyMe-Frontend/src/App.jsx
+++ MedifyMe-Frontend/src/App.jsx
@@ -2,8 +2,9 @@ import Home from "./pages/Home/Home";
 import Account from "./pages/Account/Account";
 import Manage_docters from "./pages/Manage_docters";
 import Login from "./pages/Login/Login";
-import Health_history_form from "./pages/Health_history/Health_history";
+import Health_history_form from "./pages/Health_history_form/Health_history_form";
 import Register from "./pages/Register/Register";
+import Health_history from "./pages/HealthHistory/healtH";
 // import Chatbot from "./components/Chatbot/Chatbot";
 import "./App.css";
 
@@ -23,6 +24,7 @@ const router = createBrowserRouter(
       <Route path="register" element={<Register />} />
       <Route path="manage_docters" element={<Manage_docters />} />
       <Route path="healthHistoryForm" element={<Health_history_form />} />
+      <Route path="health_history" element={<Health_history />} />
       {/* <Route path="chatbot" element={<Chatbot />} /> */}
       {/* <Route path="*" element={<Error404 />} /> */}
     </Route>
