1243fe7bfee7edfaf0355989b63ef14a7461a4ab
SaiRev <saiyamjain1234567@gmail.com>
2023-03-18 16:11:37 +0530

    Added register

--- MedifyMe-Frontend/src/App.jsx
+++ MedifyMe-Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Account from "./pages/Account/Account";
 import Manage_docters from "./pages/Manage_docters";
 import Login from "./pages/Login/Login";
 import Health_history from "./pages/Health_history/Health_history";
+import Register from "./pages/Register/Register";
 import "./App.css";
 
 import {
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
       <Route index element={<Home />} />
       <Route path="account" element={<Account />} />
       <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
       <Route path="manage_docters" element={<Manage_docters />} />
       <Route path="health_history" element={<Health_history />} />
       {/* <Route path="*" element={<Error404 />} /> */}
--- MedifyMe-Frontend/src/pages/Login/Login.jsx
+++ MedifyMe-Frontend/src/pages/Login/Login.jsx
@@ -5,16 +5,16 @@ import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { useGLogin } from "../../hooks/use-gLogin";
-import AccountCard from "../../components/AccountCard/AccountCard";
+
 function Login() {
   const navigate = useNavigate();
   const { handleGoogleLogin, loginResults } = useGLogin();
 
-  const user = useSelector((state) => {
-    return state.user;
+  const patient = useSelector((state) => {
+    return state.patient;
   });
 
-  console.log(user);
+  console.log(patient);
   console.log(loginResults);
 
   const loginHandler = useGoogleLogin({
@@ -23,65 +23,30 @@ function Login() {
     },
   });
 
-  // useEffect(() => {
-  //   if (
-  //     user.isLoggedIn &&
-  //     loginResults.data &&
-  //     loginResults.data.status === 212
-  //   ) {
-  //     navigate("/register");
-  //     toast.warn(loginResults.data.message);
-  //   }
+  useEffect(() => {
+    if (
+      patient.isLoggedIn &&
+      loginResults.data &&
+      loginResults.data.status === 212
+    ) {
+      navigate("/register");
+      toast.warn(loginResults.data.message);
+    }
 
-  //   if (loginResults.data && loginResults.data.status === 210) {
-  //     toast.warn(loginResults.data.message);
-  //   }
-
-  //   if (
-  //     user.isLoggedIn &&
-  //     loginResults.data &&
-  //     loginResults.data.status === 200
-  //   ) {
-  //     navigate("/");
-  //     toast.success(`Welcome ${user.name}`);
-  //   }
-  // }, [loginResults.data, navigate, user.isLoggedIn, user.name]);
+    if (
+      patient.isLoggedIn &&
+      loginResults.data &&
+      loginResults.data.status === 200
+    ) {
+      navigate("/");
+      toast.success(`Welcome ${patient.name}`);
+    }
+  }, [loginResults.data, navigate, patient.isLoggedIn, patient.name]);
 
   return (
-    <div className={styles.grid_container}>
-        <div className={styles.left_wrapper}>
-        <div className={styles.card}>
-        <div>
-          <h2 className={styles.content_left_wrapper}>Manage your Health,not just your Records</h2>
-        </div>
-        <img src="Group 9424.png" alt="" className={styles.img2} />
-      </div>
-        </div>
-        <div className={styles.right_wrapper}>
-         <h4 className={styles.login_content}>Verify Yourself to Proceed Further</h4>
-         <div className={styles.for_inline}>
-
-
-         <label className={styles.patients_input} htmlFor="sex">
-                I am a:
-              </label>
-              <select
-                className={styles.input_text1}
-                id="sex"
-                name="sex"
-                required
-              >
-                <option value=""></option>
-                <option value="male">Patient</option>
-                <option value="male">Docter</option>
-              </select>
-            
-
-
-         </div>
-        
-        </div>
-      </div>
+    <button disabled={loginResults.isLoading} onClick={() => loginHandler()}>
+      Signin
+    </button>
   );
 }
 
