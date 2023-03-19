7c2137c8a22a1a8b95145f022e68bdf9dd887bc5
SaiRev <saiyamjain1234567@gmail.com>
2023-03-19 05:52:27 +0530

    Fixes

--- MedifyMe-Frontend/src/pages/Health_history_form/Health_history_form.jsx
+++ MedifyMe-Frontend/src/pages/Health_history_form/Health_history_form.jsx
@@ -1,34 +1,89 @@
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./Health_history_form.module.css";
 import Footer from "../../components/Footer/Footer";
-
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [doctorName, setDoctorName] = useState("");
+  const [date, setDate] = useState("");
+  const [doctorComments, setDoctorComments] = useState("");
+  const [patientComments, setPatientComments] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    let finalData = {
+      doctorName,
+      date,
+      doctorComments,
+      patientComments,
+    };
+  };
+
   return (
     <>
       <Navbar />
-     <div className={styles.health_history}>
+      <div className={styles.health_history}>
+        <form onSubmit={handleSubmit} className={styles.health_history_form}>
+          <h1 className={styles.header}>Health History Form</h1>
+          <label className={styles.docter_name} htmlFor="doctor-name">
+            Doctor Name:
+          </label>
+          <input
+            className={styles.health_input}
+            type="text"
+            id="doctor-name"
+            name="doctor-name"
+            value={doctorName}
+            onChange={(e) => setDoctorName(e.target.value)}
+            required
+          />
+          <label className={styles.text_health} htmlFor="date">
+            Date:
+          </label>
+          <input
+            className={styles.health_input}
+            type="date"
+            id="date"
+            name="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+          />
+          <label className={styles.text_health} htmlFor="doctor-comments">
+            Doctor Comments:
+          </label>
+          <textarea
+            value={doctorComments}
+            onChange={(e) => setDoctorComments(e.target.value)}
+            id="doctor-comments"
+            name="doctor-comments"
+          ></textarea>
 
-     <form className={styles.health_history_form}>
-  <h1 className={styles.header}>Health History Form</h1>
-  <label className={ styles.docter_name}  htmlFor="doctor-name">Doctor Name:</label>
-  <input className={styles.health_input} type="text" id="doctor-name" name="doctor-name" required />
-  <label  className={ styles.text_health}  htmlFor="date">Date:</label>
-  <input className={styles.health_input} type="date" id="date" name="date" required />
-  <label  className={ styles.text_health}  htmlFor="doctor-comments">Doctor Comments:</label>
-  <textarea id="doctor-comments" name="doctor-comments"></textarea>
+          <label className={styles.text_health} htmlFor="patient-comments">
+            Patient Comments:
+          </label>
+          <textarea
+            value={patientComments}
+            onChange={(e) => setPatientComments(e.target.value)}
+            id="patient-comments"
+            name="patient-comments"
+          ></textarea>
 
-  <label   className={ styles.text_health} htmlFor="patient-comments">Patient Comments:</label>
-  <textarea id="patient-comments" name="patient-comments"></textarea>
+          <label className={styles.text_health} htmlFor="file-upload">
+            Upload Files:
+          </label>
+          <input
+            className={styles.health_file}
+            type="file"
+            id="file-upload"
+            name="file-upload"
+          />
 
-  <label  className={ styles.text_health}  htmlFor="file-upload">Upload Files:</label>
-  <input className={styles.health_file} type="file" id="file-upload" name="file-upload" />
-
-  <button className={styles.submit_button}type="submit">Submit</button>
-</form>
-
-
-     </div>
+          <button className={styles.submit_button} type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
       <Footer />
     </>
   );
