d8a4c1fb1c932e4234ed9d98e2e6afa6f1713790
SaiRev <saiyam.jain.mec21@itbhu.ac.in>
2023-03-30 20:18:52 +0530

    Created Proxy

--- MedifyMe-Frontend/vite.config.js
+++ MedifyMe-Frontend/vite.config.js
@@ -1,9 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
+import httpProxy from "http-proxy";
+
 dotenv.config();
 
-// https://vitejs.dev/config/
+function createMyProxy() {
+  return httpProxy.createProxy({
+    target: "https://medifyme-backend.onrender.com",
+    changeOrigin: true,
+    pathRewrite: { "^/server": "" },
+  });
+}
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/server": {
+        target: "https://medifyme-backend.onrender.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/server/, ""),
+        createProxy: createMyProxy,
+      },
+    },
+  },
+});
