5ce8508ecbc378ccbc3917100a2f7bc57bc2445e
SaiRev <saiyam.jain.mec21@itbhu.ac.in>
2023-04-05 01:39:04 +0530

    Added logout hook

--- MedifyMe-Frontend/src/components/AccountCard/AccountCard.jsx
+++ MedifyMe-Frontend/src/components/AccountCard/AccountCard.jsx
@@ -1,19 +1,26 @@
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styles from "./AccountCard.module.css";
 
 function AccountCard() {
   const location = useLocation();
+  const patient = useSelector((state) => {
+    return state.patient;
+  });
+
+  const customId = () => {
+    const smallRandomId = patient.id.substring(0, 8);
+    const prefixCode = patient.role === "patient" ? "PA" : "DR";
+    const Id = `${prefixCode}: #${smallRandomId.toUpperCase()}`;
+    return Id;
+  };
 
   return (
     <div className={styles.left_wrapper}>
       <div className={styles.card}>
-        <img
-          src={import.meta.env.BASE_URL + "Ellipse250.png"}
-          alt=""
-          className={styles.img}
-        />
+        <img src="../Ellipse250.png" alt="profile" className={styles.img} />
         <div className={styles.list}>
-          <h2 className={styles.id}>ID: #SA2347OLF</h2>
+          <h2 className={styles.id}></h2>
           <p className={styles.margin_top}>
             <Link
               className={
